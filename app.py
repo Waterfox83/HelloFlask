b7066e2e290a11223625a239a440710e23f6b697
Abhishek Asthana <abhishek.asthana@blueyonder.com>
2020-10-03 01:23:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/")
+def hello():
+    return "<html><body><h1>Hello Best Bike App!</h1></body></html>\n"
 
 
 @app.route('/hello')
