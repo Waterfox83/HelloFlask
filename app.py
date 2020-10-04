ead220785d63b2df6eee7e0f1b25430bbaa15b68
Abhishek Asthana <abhishek.asthana@blueyonder.com>
2020-10-04 15:02:37 +0530

    Using environment variables

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "<html><body><h2>Hello New App!</h2></body></html>\n"
+    hero_name = os.environ.get('hero_name', 'Maeve')
+    superhero_name = os.environ.get('superhero_name', 'Homelander')
+    welcome_string = "Hello "+hero_name+" and " + superhero_name
+    print(welcome_string)
+    return "<html><body><h2>"+ welcome_string + "</h2></body></html>\n"
 
 
 @app.route('/hello')
